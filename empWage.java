ecaefccda3f481fb8121cd0068d579c23415fe8e
gangadevi <kakarlagangadevi@gmail.com>
2020-07-09 20:41:21 +0530

    Employee wage for a month computed

--- empWage.java
+++ empWage.java
@@ -4,10 +4,13 @@ public class empWage
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
 	public static final int WAGE_PER_HOUR=20;
+	public static final int NUM_WORKING_DAYS=20;
 	public static void main(String args[])
 	{
 		//variables
-		int empWage=0,empHours=0;
+		int empWage=0,empHours=0,totalempWage=0;
+		for(int day=1;day<=NUM_WORKING_DAYS;day++)
+		{
 		double empCheck=Math.floor(Math.random()*10)%3; 
 		switch((int)empCheck)
 		{
@@ -21,6 +24,9 @@ public class empWage
 								empHours=0;
 		}
 		empWage=empHours*WAGE_PER_HOUR;
-		System.out.println("Employee daily wage"+empWage);
+		totalempWage+=empWage;
+		System.out.println("Day"+day+"Employee daily wage"+empWage);
+		}
+		System.out.println("Total Employee Wage is"+totalempWage);
 	}
 }
