1106ab20ffd23c905add7964e5854065921428c4
gangadevi <kakarlagangadevi@gmail.com>
2020-07-09 15:56:19 +0530

    Employee wage calculated using Switch Statement

--- empWage.java
+++ empWage.java
@@ -1,19 +1,26 @@
 public class empWage
 {
+		//constants
+	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=2;
+	public static final int WAGE_PER_HOUR=20;
 	public static void main(String args[])
 	{
-	int IS_FULL_TIME=1;
-	int IS_PART_TIME=2;
-	int WAGE_PER_HOUR=20;
-	int salary=0,empHours=0;
-	double empCheck=Math.floor(Math.random()*10)%3; 
-	if(empCheck==IS_FULL_TIME)
-			empHours=8;
-	else if(empCheck==IS_PART_TIME)
-			empHours=4;
-	else
-		System.out.println("Employee is Absent");
-	salary=empHours*WAGE_PER_HOUR;
-	System.out.println("Employee daily wage"+salary);
+		//variables
+		int empWage=0,empHours=0;
+		double empCheck=Math.floor(Math.random()*10)%3; 
+		switch((int)empCheck)
+		{
+			case IS_FULL_TIME:
+								empHours=8;
+								break;
+			case IS_PART_TIME:
+								empHours=4;
+								break;
+			default:
+								empHours=0;
+		}
+		empWage=empHours*WAGE_PER_HOUR;
+		System.out.println("Employee daily wage"+empWage);
 	}
 }
