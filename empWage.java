d4a9c3463977f4f18ed746af87c12e7fca5adef4
gangadevi <kakarlagangadevi@gmail.com>
2020-07-10 10:08:49 +0530

    Employee Wage Calcualted till MaxWorkHours or total working days

--- empWage.java
+++ empWage.java
@@ -5,15 +5,17 @@ public class empWage
 	public static final int IS_PART_TIME=2;
 	public static final int WAGE_PER_HOUR=20;
 	public static final int NUM_WORKING_DAYS=20;
+	public static final int MAX_WORK_HOURS=100;
 	public static void main(String args[])
 	{
 		//variables
-		int empWage=0,empHours=0,totalempWage=0;
-		for(int day=1;day<=NUM_WORKING_DAYS;day++)
+		int empWage=0,empHours=0,totalempHours=0,totalempWage=0,totalWorkingDays=0;
+		while(totalempHours<=MAX_WORK_HOURS && totalWorkingDays<NUM_WORKING_DAYS)
 		{
-		double empCheck=Math.floor(Math.random()*10)%3; 
-		switch((int)empCheck)
-		{
+			totalWorkingDays++;
+			double empCheck=Math.floor(Math.random()*10)%3; 
+			switch((int)empCheck)
+			{
 			case IS_FULL_TIME:
 								empHours=8;
 								break;
@@ -22,10 +24,11 @@ public class empWage
 								break;
 			default:
 								empHours=0;
-		}
-		empWage=empHours*WAGE_PER_HOUR;
-		totalempWage+=empWage;
-		System.out.println("Day"+day+"Employee daily wage"+empWage);
+			}
+		//empWage=empHours*WAGE_PER_HOUR;
+		//System.out.println("Day"+totalWorkingDays+"Employee daily wage"+empWage);
+		totalempHours+=empHours;
+		totalempWage=totalempHours*WAGE_PER_HOUR;
 		}
 		System.out.println("Total Employee Wage is"+totalempWage);
 	}
